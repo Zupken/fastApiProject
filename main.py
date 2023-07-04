0de599ddd4bfc92a93c2c7e42531255dd97b1a4a
kuba <kuba@aimachine.myguest.virtualbox.org>
2023-07-04 17:05:27 +0200

    Revert to working version.

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr, validator
 import re
-import sqlite3
-
-
-
-def create_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect("phone_book.db")
-        print("Connected to SQLite database")
-        return conn
-    except Error as e:
-        print(f"Error connecting to SQLite database: {e}")
-    finally:
-        if conn:
-            conn.close()
 
 
 class Item(BaseModel):
@@ -45,31 +30,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello, FastAPI!"}
 
-@app.get("/phone_book")
-def get_phone_book():
-    conn = create_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM phone_book")
-    rows = cursor.fetchall()
-    phone_book = []
-    for row in rows:
-        entry = {
-            "id": row[0],
-            "first_name": row[1],
-            "last_name": row[2],
-            "number": row[3],
-            "email": row[4]
-        }
-        phone_book.append(entry)
-    return phone_book
 
 @app.post("/item/")
 async def create_item(item: Item):
-    conn = create_connection()
-    cursor = conn.cursor()
-    cursor.execute(
-            "INSERT INTO phone_book (first_name, last_name, number, email) VALUES (?, ?, ?, ?)",
-            (item.first_name, item.last_name, item.number, item.email)
-        )
-    conn.commit()
     return item
