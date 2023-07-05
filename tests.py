fd7f7a7e71c27806ba1e51cc5a97a9aaffb62293
kuba <kuba@aimachine.myguest.virtualbox.org>
2023-07-05 14:13:58 +0200

    Added more create tests.

--- tests.py
+++ tests.py
@@ -10,7 +10,7 @@ from database import SessionLocal, engine, Base
 
 
 
-class MyTestCase(unittest.TestCase):
+class TestCreatingEntry(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -18,16 +18,10 @@ class MyTestCase(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        # Create a new connection to the database.
-        print('end of tests')
         with engine.connect() as connection:
-            # Begin a new transaction.
             with connection.begin() as transaction:
-                # Run a raw SQL query to delete all entries from the table.
                 for table in Base.metadata.tables.values():
                     connection.execute(table.delete())
-
-
 
     def setUp(self):
         self.db = SessionLocal()
@@ -49,20 +43,73 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(entry.last_name, "Doe")
         self.assertEqual(entry.number, "+1234567890")
         self.assertEqual(entry.email, "john.doe@example.com")
-
-    def test_create_entry_invalid(self):
+    
+    def test_create_entry_max_long_number(self):
         entry_data = {
             "first_name": "Jane",
             "last_name": "Smith",
-            "number": "123",
+            "number": "+123456789012345",
+            "email": "jane.smith@example.com"
+        }
+        response = self.client.post("/entry/", json=entry_data)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_create_entry_min_long_number(self):
+        entry_data = {
+            "first_name": "John",
+            "last_name": "Smith",
+            "number": "+12",
+            "email": "john.smith@example.com"
+        }
+        response = self.client.post("/entry/", json=entry_data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_entry_no_country_code(self):
+        entry_data = {
+            "first_name": "Jane",
+            "last_name": "Smith",
+            "number": "1234242",
             "email": "jane.smith@example.com"
         }
         response = self.client.post("/entry/", json=entry_data)
         self.assertEqual(response.status_code, 422)
+    
+    def test_create_entry_too_short_number(self):
+        entry_data = {
+            "first_name": "Jane",
+            "last_name": "Smith",
+            "number": "+1",
+            "email": "jane.smith@example.com"
+        }
+        response = self.client.post("/entry/", json=entry_data)
+        self.assertEqual(response.status_code, 422)
+    
+    def test_create_entry_too_long_number(self):
+        entry_data = {
+            "first_name": "Jane",
+            "last_name": "Smith",
+            "number": "+1234567890123456",
+            "email": "jane.smith@example.com"
+        }
+        response = self.client.post("/entry/", json=entry_data)
+        self.assertEqual(response.status_code, 422)
+    
+    def test_create_entry_bad_email(self):
+        entry_data = {
+            "first_name": "Jane",
+            "last_name": "Smith",
+            "number": "123",
+            "email": "jane.smith"
+        }
+        response = self.client.post("/entry/", json=entry_data)
+        self.assertEqual(response.status_code, 422)
+    
+
+
 
 def suite():
     test_suite = unittest.TestSuite()
-    test_suite.addTest(unittest.makeSuite(MyTestCase))
+    test_suite.addTest(unittest.makeSuite(TestCreatingEntry))
     return test_suite
 
 if __name__ == '__main__':
